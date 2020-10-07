fc2d9edbb49b5d7b307e28a5275d691e85d4b666
Pelleteret <clelia.pelleteret@hawk-platform.io>
2020-10-07 09:09:12 +0200

    feat(App): create Welcome page and add test for app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-import reducers from './app/reducer/index';
+import reducers from './app/reducer/index'
 import GameBoard from './app/components/GameBoard.js'
 import logo from './app/styles/img/logoCineQuiz.png'
 import './App.css';
 
 const store = createStore(reducers, (applyMiddleware(thunk)));
 
-const App= () => {
+class App extends React.Component {
+
+  state = {
+    beginGame: false
+  }
+
+  changeBeginGameValue= () =>{
+    this.setState({beginGame: true})
+  }
+  
+  render () {
     return (
       <Provider store={store}>
         <div className="App">
@@ -18,12 +28,26 @@ const App= () => {
             <span className="App-header_logo"><img src={logo} alt="LogoCineQuiz"></img></span>
             <span className="App-header_text">Clélia Pelleteret</span> 
           </header>
-          <div>
-            <GameBoard></GameBoard>
+          <div className="App-body">
+          {
+            this.state.beginGame 
+            ? <GameBoard></GameBoard>
+            : <>
+                <div className="App-body_logo"><img src={logo} alt="LogoCineQuiz"></img></div>
+                <div className="App-body_rules">
+                   <h2> Welcome to the quizz ! </h2>
+                  <span className="pt5"> You'll be asked a series of <span className="text-bold">"Yes or No"</span> questions.</span>
+                  <span className="pt5">Answer as many as you can in the allowed time ! </span>
+                  <span className="pt5">Good luck !</span>
+                </div>
+                <button className="App-body_button" onClick={this.changeBeginGameValue}>Let's Start</button>
+              </>
+          }
           </div>
         </div>
       </Provider>
-    );
+    )
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App.js'
+import GameBoard from './app/components/GameBoard.js'
+
+describe('App test', () => {
+
+  test('App renders correctly', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('.App')).toHaveLength(1)
+    expect(wrapper.find('.App-header')).toHaveLength(1)
+    expect(wrapper.find('.App-header_logo')).toHaveLength(1)
+    expect(wrapper.find('.App-header_text')).toHaveLength(1)
+    expect(wrapper.find('.App-body')).toHaveLength(1)
+    expect(wrapper.find('.App-body_logo')).toHaveLength(1)
+    expect(wrapper.find('.App-body_rules')).toHaveLength(1)
+    expect(wrapper.find('.App-body_button')).toHaveLength(1)
+    wrapper.unmount()
+  })
+
+  test('App renders correctly if beginGame is true', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App')).toHaveLength(1)
+    expect(wrapper.find('.App-header')).toHaveLength(1)
+    expect(wrapper.find('.App-header_logo')).toHaveLength(1)
+    expect(wrapper.find('.App-header_text')).toHaveLength(1)
+    expect(wrapper.find('.App-body')).toHaveLength(1)
+    wrapper.setState({ beginGame: true });
+    expect(wrapper.state('beginGame')).toEqual(true)
+    expect(wrapper.find(<GameBoard/>))
+    wrapper.unmount()
+  })
+
+})
